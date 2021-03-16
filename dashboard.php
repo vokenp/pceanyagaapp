df03e388b86336e79adeabeb2e7b12730f738983
Kevin Mungai <vokenp@gmail.com>
2021-03-16 09:20:13 +0300

    Fixed Search Bug

---
+++ dashboard.php
@@ -0,0 +1,32 @@
+<div class="page-header">
+  <h1>
+    Dashboard
+    <small>
+      <i class="ace-icon fa fa-angle-double-right"></i></small>
+  
+  </h1>
+  
+</div><!-- /.page-header -->
+<div class="page-content">
+  <div class="row">
+    
+    <div class="row">
+				<div class="space-6"></div>
+				<div class="col-sm-7 infobox-container">
+					<div class="infobox infobox-green">
+						<div class="infobox-icon">
+							<i class="ace-icon fa fa-comments"></i>
+						</div>
+
+						<div class="infobox-data">
+							<span class="infobox-data-number">32</span>
+							<div class="infobox-content">comments + 2 reviews</div>
+						</div>
+
+						<div class="stat stat-success">8%</div>
+					</div>
+
+			   </div><!-- End InfoBox Container -->
+
+   </div>
+ </div>
